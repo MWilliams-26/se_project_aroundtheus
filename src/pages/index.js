e32608ee2e447460f1c2c2b7e6333843db077f95
MWilliams-26 <129562325+MWilliams-26@users.noreply.github.com>
2024-01-30 02:22:55 -0500

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,10 @@ import Section from "../components/Section";
 import Popup from "../components/Popup";
 import PopupWithForm from "../components/PopupWithForm";
 import PopupWithImage from "../components/PopupWithImage";
+import PopupWithConfirmation from "../components/PopupWithConfirmation";
 import UserInfo from "../components/UserInfo";
 import Api from "../components/Api";
+import { data } from "autoprefixer";
 
 
 // Profile Edit
@@ -36,14 +38,15 @@ const modalImageElement = imagePreviewModal.querySelector(".modal__image-preview
 const modalCaption = imagePreviewModal.querySelector(".modal__image-caption");
 const imagePreviewCloseButton = imagePreviewModal.querySelector(".modal__close");
 
+// Confirm Delete Modal
+const trashIcon = document.querySelector("#confirm-delete-modal");
+const trashSubmitButton = trashIcon.querySelector("#confirm-delete-submit")
 
 
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
 
 
 
-
-// Create instances of the classes
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
   headers: {
@@ -52,60 +55,91 @@ const api = new Api({
   }
 }); 
 
-const cardSection = new Section({
-  items: initialCards,
-  renderer: (cardData) => {
-    const cardEl = renderCard(cardData);
-    cardSection.addItem(cardEl);
-  },
-},  
-".cards__list"
-);
+let cardSection;
 
-cardSection.renderItems();
+api
+  .loadPageContent()
+  .then((res) => {
+    cardSection = new Section({
+      items: initialCards,
+      renderer: (cardData) => {
+        const cardEl = renderCard(cardData);
+        cardSection.addItem(cardEl);
+      },
+    },  
+  ".cards__list"
+  );
+  
+  cardSection.renderItems();
+  })
+  .catch((err) => {
+    console.error(err); 
+  });
 
-const profileEditFormPopup = new PopupWithForm("#profile-edit-modal", handleProfileEditSubmit);
-profileEditFormPopup.setEventListeners();
 
-const editFormElement = profileEditModal.querySelector(".modal__form_edit");
-const editFormValidator = new FormValidator(validationSettings, editFormElement);
-editFormValidator.enableValidation();
 
-const profileAddCardPopup = new PopupWithForm("#profile-add-card-modal", handleProfileAddCardSubmit);
-profileAddCardPopup.setEventListeners();
 
-const addFormElement = profileAddCardModal.querySelector(".modal__form_add");
-const addFormValidator = new FormValidator(validationSettings, addFormElement);
-addFormValidator.enableValidation();
+const editFormElement = profileEditModal.querySelector(".modal__form_edit");  
+const editFormValidator = new FormValidator(validationSettings, editFormElement);  
+editFormValidator.enableValidation();  
+
+const addFormElement = profileAddCardModal.querySelector(".modal__form_add");  
+const addFormValidator = new FormValidator(validationSettings, addFormElement);  
+addFormValidator.enableValidation();  
+
+
+const popupWithImage = new PopupWithImage("#image-preview-modal");  
+popupWithImage.setEventListeners();
+
+function handlePreviewPicture(name, link) {
+  popupWithImage.open(name, link);
+}
 
 const profileUserInfo = new UserInfo(".profile__title", ".profile__description");
 
-const popupWithImage = new PopupWithImage("#image-preview-modal");
-popupWithImage.setEventListeners();
-
-// Initialize all my instances
-
-
-// all the rest
 function renderCard(cardData) {
-  return new Card(cardData, "#card-template", () => {
-    popupWithImage.open(cardData.name, cardData.link)
-  })
-  .generateCard();
+  const card = new Card(
+    cardData, 
+    "#card-template",
+    handlePreviewPicture,
+    handleDeleteClick,
+    handleLikeClick,
+  );
+  return card.generateCard();
 }
 
+const profileEditFormPopup = new PopupWithForm("#profile-edit-modal", handleProfileEditSubmit);  
+profileEditFormPopup.setEventListeners();
 
 function handleProfileEditSubmit(inputValues) {
-  profileUserInfo.setUserInfo(inputValues.title, inputValues.description);
-  profileEditFormPopup.close();
+  api
+  .editProfileInfo(inputValues)
+  .then((res) => {
+    profileUserInfo.setUserInfo(inputValues.title, inputValues.description);
+    profileEditFormPopup.close();
+  })
+  .catch((err) => {
+    console.error(err);
+  })
 };
 
+const profileAddCardPopup = new PopupWithForm("#profile-add-card-modal", handleProfileAddCardSubmit);  
+profileAddCardPopup.setEventListeners();
+
 function handleProfileAddCardSubmit(inputValues) {
-  const cardData = renderCard({name: inputValues.title, link: inputValues.url });
-  cardSection.addItem(cardData);
-  profileAddCardPopup.close();
-  
+  api
+    .addCard(inputValues)
+    .then((res) => {
+      const cardData = renderCard({name: inputValues.title, link: inputValues.url });
+      cardSection.addItem(cardData);
+      profileAddCardPopup.close();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
+
+
 
 profileEditButton.addEventListener("click", () => {
   editFormValidator.resetValidation();
@@ -121,3 +155,42 @@ profileAddCardButton.addEventListener("click", () => {
   addFormValidator.resetValidation();
 });
 
+function handleLikeClick(card) {
+    if (card.isLiked) {
+      api
+      .likeCard(card.getId())
+      .then((res) => {
+        card.handleLikeClick();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    } else {
+      api
+      .unlikeCard(card.getId())
+      .then((res) => {
+        card.handleLikeClick();
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    }
+}  
+  
+const confirmDelete = new PopupWithConfirmation("#confirm-delete-modal");
+  confirmDelete.setEventListeners();
+  
+function handleDeleteClick(cardId) {
+  confirmDelete.open();
+  confirmDelete.setSubmitAction(() => {  
+    api
+      .deleteCard(cardId)
+      .then(() => {
+        cardId.handleDeleteClick();
+        confirmDelete.close();
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    });
+}        
